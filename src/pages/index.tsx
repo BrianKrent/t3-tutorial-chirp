bb012f785bff2c57eb0e8ac56e824d6a41cf414c
Brian Krent <briankrent@mac.com>
2023-04-07 08:02:38 -0400

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,8 @@ import { api } from "~/utils/api";
 
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
- 
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -21,6 +20,9 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
